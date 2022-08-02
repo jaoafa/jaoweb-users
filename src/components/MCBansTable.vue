07efd7c51065a2ebf62f86af2413fb7f01ed162c
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-08-02 19:45:32 +0900

    fix: #433 MCBans欄が無限くるくるする問題を修正

--- src/components/MCBansTable.vue
+++ src/components/MCBansTable.vue
@@ -178,6 +178,8 @@ export default Vue.extend({
               this.gbanCount = 0
               this.lbanCount = 0
               this.reputation = 10
+              this.updateAt = this.getDateTime(new Date())
+              this.loading = false
               return
             }
             DataStore.showModal({
@@ -212,6 +214,15 @@ export default Vue.extend({
             })
           })
       })
+    },
+    getDateTime(date: Date) {
+      const year = date.getFullYear()
+      const month = ('00' + (date.getMonth() + 1).toString()).slice(-2)
+      const day = ('00' + date.getDate().toString()).slice(-2)
+      const hour = ('00' + date.getHours().toString()).slice(-2)
+      const minute = ('00' + date.getMinutes().toString()).slice(-2)
+      const second = ('00' + date.getSeconds().toString()).slice(-2)
+      return `${year}-${month}-${day} ${hour}:${minute}:${second}`
     },
   },
 })
